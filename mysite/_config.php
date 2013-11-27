e0e24c52d97fe787e9ca8202d10202cce4ca71cf
Mario Michelli <mario@edison.no>
2013-11-27 13:49:27 +0100

    code from stig

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,7 +9,7 @@ $database = 'SS_mysite';
 require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
-i18n::set_locale('en_US');
+
 SiteConfig::add_extension('SitelSiteConfig');
 Image::add_extension('GalleryImageExtension');
 File::add_extension('PageFilesExtension');
